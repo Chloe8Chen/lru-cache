ff1314bca8d16d1edb96bc561066d150080926e6
chloeChenChen <chloe@ingenius.com>
2020-09-27 21:05:38 -0400

    the code is working, tests and docs needed

--- LruCache.js
+++ LruCache.js
@@ -1,24 +1,51 @@
+// A Least Recently Used Cache
 class LruChache
 {
+    // constructor that defines:
+    //      * size of the cache
+    //      * reset the members
     constructor(size)
     {
         this.size = size;
-        this.totalNumberOfItems = 0;
+        this.reset();
+    }
+
+    reset()
+    {
+        this.storage = {};
         this.head = new LinkNode();
         this.tail = new LinkNode();
         this.head.next = this.tail;
         this.tail.prev = this.head;
-        this.storage = {};
+        this.totalNumberOfItems = 0;        
+    }
+
+    del(key)
+    {
+        if (this.storage[key])
+        {
+            let oldNext = this.storage[key].next;
+            let oldPrev = this.storage[key].prev;
+            this.storage[key].prev.next = oldNext;
+            this.storage[key].next.prev = oldPrev;
+            delete this.storage[key];
+            this.totalNumberOfItems--;
+        }
+    }
+    
+    _addToFront(node)
+    {
+        node.prev = this.head;
+        node.next = this.head.next;
+        this.storage[node.key] = node;
+        this.head.next.prev = node;
+        this.head.next = node;
     }
 
     get(key)
     {
         let value = null;
-        if (!this.storage[key])
-        {
-            console.log(key + " does not exist in cache");
-        }
-        else
+        if (this.storage[key])
         {
             value = this.storage[key].value;
             let newNode = new LinkNode(key, value);
@@ -31,6 +58,8 @@ class LruChache
 
     put(key, value)
     {
+        // if the key already exists, delete it first so we can add it back to the head of the list later
+        this.del(key);
         let newNode = new LinkNode(key, value);
         this._addToFront(newNode);
         this.totalNumberOfItems++;
@@ -39,43 +68,6 @@ class LruChache
             this.del(this.tail.prev.key);
             this.totalNumberOfItems--;
         }
-    }
-
-    del(key)
-    {
-        if (!this.storage[key])
-        {
-            console.log("key " + key + " does not exist!");
-        }
-        else
-        {
-            let oldNext = this.storage[key].next;
-            let oldPrev = this.storage[key].prev;
-            this.storage[key].prev.next = oldNext;
-            this.storage[key].next.prev = oldPrev;
-            console.log("deleting [" + key + ", " + this.storage[key].value + "] from cache!");
-            delete this.storage[key];
-            this.totalNumberOfItems--;
-        }
-    }
-    
-    reset()
-    {
-        this.storage = {};
-        this.head = new LinkNode();
-        this.tail = new LinkNode();
-        this.head.next = this.tail;
-        this.tail.prev = this.head;
-        this.totalNumberOfItems = 0;        
-    }
-
-    _addToFront(node)
-    {
-        node.prev = this.head;
-        node.next = this.head.next;
-        this.storage[node.key] = node;
-        this.head.next.prev = node;
-        this.head.next = node;
     }
 
 }
@@ -92,12 +84,21 @@ class LinkNode
 
 function test()
 {
-    let cache = new LruChache(7);
+    let cache = new LruChache(3);
     cache.put(1,2);
     cache.put(3,4);
+    cache.put(5,6);
+    cache.get(1);
+    cache.put(6,8);
+    cache.del(6);
+    cache.reset();
+    cache.put(1,2);
+    cache.put(3,4);
+    cache.put(5,6);
+    cache.get(1);
+    cache.put(6,8);
     console.log(cache.get(1));
-//    cache.del(3);
-  //  console.log(cache.get(1));
-    console.log(cache);
+    console.log(cache.get(6));
+
 }
 test();
